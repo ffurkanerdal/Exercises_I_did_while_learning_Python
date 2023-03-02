ba5a3854168880baff484d0eebb59020ff650f25
ffurkanerdal <118114461+ffurkanerdal@users.noreply.github.com>
2023-03-02 18:07:24 +0300

    Update hesapmak.py

--- hesapmak.py
+++ hesapmak.py
@@ -1,44 +1,88 @@
-# import math
-# x=int(input("Sayı gir"))
-# y=int(input("Sayı gir"))
-# islem=input("işlem gir")
-# print("""
-# Hesap Makinesi
-# +  Topla        -  Çıkar        /  Böl      // Tam Böl
-# %  Yüzde        ** Kare         2n Kök      !  Faktöriyel
-# """)
-# if islem=="+":
-#     print(x+y)
-# elif islem=="-":
-#     sonuc=x-y
-#     if sonuc<0:
-#         sonuc*=-1
-#         print(sonuc)
-#     else:
-#         print(sonuc)
-# elif islem=="/":
-#     if x>y:
-#         print(x/y)
-#     else:
-#         print(y/x)
-# elif islem=="//":
-#     if x>y:
-#         print(x//y)
-#     else:
-#         print(y//x)
-# elif islem=="%":
-#     s=(x/100)*y
-# elif islem=="**":
-#     print(x**x)
-# elif islem=="2n":
-#     print(math.sqrt(x))
-# elif islem=="!":
-#     print(math.factorial(x))
-# else:
-#     print("hatalı veri girişi")
+#! HESAP MAKİNESİ
 
+def hesap_makinesi(islem):
+    if islem=="+":
+        def topla():
+            try:    
+                sayilar=[]
+                while True:
+                    sayi=int(input("Sayı giriniz"))
+                    sayilar.append(sayi)
+                    if sayi==0:
+                        sayilar.pop(-1)
+                        break
+                y=0
+                for i in sayilar:
+                    y+=i
+            except ValueError:
+                print("Hatalı Değer")
+            except TypeError:
+                print("Hatalı Değer")
+            except KeyError:
+                print("Hatalı Değer")
+            except SyntaxError:
+                print("Hatalı Değer")    
+            print(y)
+        topla()
 
+    if islem=="-":
+        def cikar():
+            sayilar=[]
+            while True:
+                sayi=int(input("Sayı Giriniz"))
+                sayilar.append(sayi)
+                if sayi==0:
+                    sayilar.pop(-1)
+                    break
+            a=sorted(sayilar)
+            s=a[-1]
+            for i in sayilar[1:]:
+                s-=i
+            print(s)
 
-print(bool(0))
-print(bool(""))
-print(bool("hello"))
+        cikar()
+    if islem=="*":
+        def carp():
+            try:
+                sayilar=[]
+                while True:
+                    sayi=int(input("Sayı Giriniz"))
+                    sayilar.append(sayi)
+                    if sayi==0:
+                        sayilar.pop(-1)
+                        break
+                y=1
+                for i in sayilar:
+                    y*=i
+            except ValueError:
+                print("Hatalı Değer")
+            except TypeError:
+                print("Hatalı Değer")
+            except KeyError:
+                print("Hatalı Değer")
+            except SyntaxError:
+                print("Hatalı Değer")
+            print(y)
+        carp()
+    if islem=="/":
+        def bol():
+            try:
+                sayi1=int(input("Sayı giriniz"))
+                sayi2=int(input("Sayı giriniz"))
+            except ValueError:
+                print("Hatalı Değer")
+            except TypeError:
+                print("Hatalı Değer")
+            except KeyError:
+                print("Hatalı Değer")
+            except SyntaxError:
+                print("Hatalı Değer")
+            except ZeroDivisionError:
+                print("Sıfıra Bölünemez")            
+            print(sayi1/sayi2)
+        bol()
+while True:
+    islem=input("işlem: ")
+    hesap_makinesi(islem)
+    if islem=="0":
+        break
